31b7ac21cacca2d8afec9260b64c7d794d67c1fc
Chiedo <chiedo@chiedo.com>
2018-11-07 06:48:06 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Trello List Time Totals
+
+For any list that ends with "Time:", this chrome extension will find all labels titled (Xm) up so you know how much time's worth of tasks you have in a given list.
+
+## Installation
+
+1. Git clone this repository.
+2. Go to "Extensions" in Google Chrome
+3. Turn on "Developer Mode"
+4. Click on "Load Unpacked"
+5. Load the director of this repository on your local machine
+
---
+++ content.js
@@ -0,0 +1,18 @@
+console.log("Chrome Trello List Time Totals.");
+
+$(document).ready(function(){
+  console.dir($);
+  console.dir(jQuery);
+  $('.js-list-content').each(function(){
+    var listTitle = $(this).find('.list-header-name-assist');
+
+    var timeCount;
+
+    if(listTitle.text().endsWith("Time :")) {
+      $(this).find('.js-list-content .list-cards .list-card .list-card-labels .card-label')
+        .each(function(){
+          console.dir($(this).text());
+        });
+    }
+  });
+});
